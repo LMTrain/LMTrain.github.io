ef4567a21dceb46c35190ce9f859a345c35b1280
LM Train <layconmm@msn.com>
2019-04-27 13:27:14 -0500

    Comment out all the console.log in game.js file

--- wordgame/assets/javascript/game.js
+++ wordgame/assets/javascript/game.js
@@ -19,7 +19,7 @@ function startFunction() {
   document.getElementById("submit-button").innerHTML = "Submit";
   computerGuessR.push(computerGuess);
   userInputN = userInputN + 1;
-  console.log(computerGuess + " -- This Computer Guess " + "computerGuessR =  " + computerGuessR[userInputN] + "  userInputN =  " + userInputN);
+  // console.log(computerGuess + " -- This Computer Guess " + "computerGuessR =  " + computerGuessR[userInputN] + "  userInputN =  " + userInputN);
   document.getElementById("user-text-input").innerHTML = "Complete Artist Name and click Submit:";
   document.getElementById("total-wins").textContent = "Wins = " + wins;
   document.getElementById("total-losses").textContent = " Loses = " + losses;
@@ -45,19 +45,19 @@ function myFunction() {
     computerGuessR.push(computerGuess);
     userInput.push(userText);
     userInputN = userInputN + 1;
-    console.log(userInput[userInputN] + "  userInput Array # =  " + userInputN);
+    // console.log(userInput[userInputN] + "  userInput Array # =  " + userInputN);
     document.getElementById("user-input").innerHTML = userInput[userInputN];
-    console.log(computerGuess + " -- This Computer Guess");
+    // console.log(computerGuess + " -- This Computer Guess");
     document.getElementById("computer-guess").innerHTML = computerGuessR[userInputN];
     document.getElementById("user-text").value = '';
     
     if (userInput[userInputN] === computerGuessR[userInputN]) {
-      console.log(userInput[userInputN] + "   " + computerGuessR[userInputN]);
+      // console.log(userInput[userInputN] + "   " + computerGuessR[userInputN]);
       wins = wins + 1;
       document.getElementById("total-wins").textContent = "Wins = " + wins;
     }
     else if (userInput[userInputN] !== computerGuessR[userInputN]) {
-      console.log(userInput[userInputN] + "   " + computerGuessR[userInputN]);
+      // console.log(userInput[userInputN] + "   " + computerGuessR[userInputN]);
       losses = losses + 1;
       document.getElementById("total-losses").textContent = " Loses = " + losses;
     }
@@ -97,20 +97,6 @@ function gamenotstarted() {
 };
 
 document.getElementById("start-button").innerHTML = "Click To Start Game";
-// document.getElementById("submit-button").style.visibility='hidden';
-// document.getElementById("submit-button").innerHTML = "  ";
-// $('#submit-button').click(
-//   function(){
-//     location.reload(true);
-//   }
-// )
-// var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-// document.getElementById("total-wins").textContent = "Wins = " + wins;
-// document.getElementById("total-losses").textContent = " Loses = " + losses;
-// document.getElementById("total-guesses").textContent = " Guesses = " + guesses;
-// document.getElementById("column3text").innerHTML = computerGuess;
-// document.getElementById("computer-guess-block").innerHTML = computerGuess;
-// document.getElementById("computer-guess").innerHTML = userInputL;
 
 
 
